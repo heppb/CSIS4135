45361b4129fc7aad86b1724a93d228e1052db0e2
Brian <Brian@BRIAN-PC>
2016-11-30 12:06:58 -0500

    Small changes to migrations and register model

--- EventManager/Models/AccountViewModels/RegisterViewModel.cs
+++ EventManager/Models/AccountViewModels/RegisterViewModel.cs
@@ -23,5 +23,8 @@ namespace EventManager.Models.AccountViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string UserIdentity { get; set; }
     }
 }
